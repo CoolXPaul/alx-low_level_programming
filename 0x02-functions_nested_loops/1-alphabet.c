468f45cf49a0fcc7744de8e113df404072f0f453
CoolXPaul <esohwodecollins@gmail.com>
2022-09-13 20:42:56 +0000

    Alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description:print_alphabet - print alphabet of letters
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	int i;
+	for
+	(i = 'a'; i <= 'z'; i++)
+
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
